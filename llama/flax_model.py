64224f435800b799db116ac3761fbf4db73c5fc4
Charlie Snell <csnell22@berkeley.edu>
2023-03-03 02:21:41 +0000

    rotary embs

--- llama/flax_model.py
+++ llama/flax_model.py
@@ -2,6 +2,7 @@ import jax
 import jax.numpy as jnp
 import flax
 import flax.linen as nn
+import numpy as np
 
 class RMSNorm(nn.Module):
     dim: int
@@ -21,13 +22,21 @@ class RMSNorm(nn.Module):
         output = self._norm(x.astype(jnp.float32)).astype(x.dtype)
         return output * self.weight
 
-# def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
-#     freqs = 1.0 / (theta ** (jnp.arange(0, dim, 2)[: (dim // 2)].astype(jnp.float32) / dim))
-#     t = jnp.arange(end) # type: ignore
-#     freqs = jnp.outer(t, freqs).float()  # type: ignore
-#     freqs_cis = jnp.polar(torch.ones_like(freqs), freqs)  # complex64
-#     return freqs_cis
+# jax rotary embs
+# from gptj https://github.com/huggingface/transformers/blob/37e0974afcbccdc85da59d51b44e1437b6b3caea/src/transformers/models/gptj/modeling_flax_gptj.py#L109
+def create_sinusoidal_positions(num_pos, dim):
+    inv_freq = 1.0 / (10000 ** (np.arange(0, dim, 2) / dim))
+    sinusoid_inp = np.einsum("i , j -> i j", np.arange(num_pos), inv_freq).astype("float32")
+    sin, cos = np.sin(sinusoid_inp), np.cos(sinusoid_inp)
 
+    sentinel = dim // 2 + dim % 2
+    out = np.zeros((num_pos, dim))
+    out[:, 0:sentinel] = sin
+    out[:, sentinel:] = cos
+
+    return jnp.array(out)
+
+# pytorch rotary embs
 # def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
 #     freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
 #     t = torch.arange(end, device=freqs.device)  # type: ignore

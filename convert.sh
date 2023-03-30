d9cb3e5ee09b9083151c4f7d49626ba0f33a3406
wimjan123 <visser.william@live.nl>
2023-03-30 23:19:45 +0200

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
-SOURCE_FOLDER="/shared/csnell/llama_weights/"             # where all llama files should come from
-TARGET_FOLDER="/shared/csnell/llama_weights_jax/"             # where all jax files should end up
-MODEL_SIZE="65B"  # edit this list with the model sizes you wish to download
+SOURCE_FOLDER="gs://gpt-j-train/LLAMA/"             # where all llama files should come from
+TARGET_FOLDER="gs://gpt-j-train/llama_weights_jax/"             # where all jax files should end up
+MODEL_SIZE="13B"  # edit this list with the model sizes you wish to download
 
 cp ${SOURCE_FOLDER}"/tokenizer.model" ${TARGET_FOLDER}
 cp ${SOURCE_FOLDER}"/tokenizer_checklist.chk" ${TARGET_FOLDER}
